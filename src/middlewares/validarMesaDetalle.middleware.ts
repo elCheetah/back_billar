8b42f724414c0b928b8df0c7c8e934635df26bc8
elCheetah <eabraham.pro@gmail.com>
2025-11-04 17:08:56 -0400

    aaa

--- src/middlewares/validarMesaDetalle.middleware.ts
+++ src/middlewares/validarMesaDetalle.middleware.ts
@@ -1,22 +1,14 @@
-import { Request, Response, NextFunction } from "express";
-import { param, validationResult } from "express-validator";
+import { param,  } from "express-validator";
 
-function resolverValidacion(req: Request, res: Response, _next: NextFunction) {
-  const errors = validationResult(req);
-  if (!errors.isEmpty()) {
-    const msg = errors.array()[0]?.msg || "Datos inválidos.";
-    return res.status(400).json({ ok: false, message: msg });
-  }
-  _next();
-}
+
 
 // Mantén estos nombres porque así los importas en las rutas
 export const validarIdLocal = [
   param("idLocal").isInt({ min: 1 }).withMessage("idLocal inválido."),
-  resolverValidacion,
+  
 ];
 
 export const validarIdMesa = [
   param("idMesa").isInt({ min: 1 }).withMessage("idMesa inválido."),
-  resolverValidacion,
+  
 ];
