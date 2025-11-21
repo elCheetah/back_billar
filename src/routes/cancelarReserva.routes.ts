2a9706a47562d2407ca4a2b45cd5c8bc3f6bdb52
elCheetah <eabraham.pro@gmail.com>
2025-11-21 02:07:12 -0400

    cambiosss

--- src/routes/cancelarReserva.routes.ts
+++ src/routes/cancelarReserva.routes.ts
@@ -1,9 +1,10 @@
 // src/routes/reserva.routes.ts
 import { Router } from "express";
 import { cancelarReservaController } from "../controllers/cancelarReserva.controller";
+import { validarCancelarReserva } from "../middlewares/validarCancelarReserva.middleware";
 
 const router = Router();
 
-router.patch("/:id_reserva", cancelarReservaController);
+router.patch("/:id_reserva", validarCancelarReserva, cancelarReservaController);
 
 export default router;
